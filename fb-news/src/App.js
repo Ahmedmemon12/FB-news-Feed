61c620d65edefecea7272b4799b537a37c763cf9
Ahmedmemon12 <135580168+Ahmedmemon12@users.noreply.github.com>
2024-01-03 10:23:00 +0500

    ..

---
+++ fb-news/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ResponsiveAppBar from './Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
